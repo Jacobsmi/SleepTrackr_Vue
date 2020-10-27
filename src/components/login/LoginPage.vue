6aba58ffe4e3c7622df8702e7a544020ba715b01
jacobsm84 <jacobsm84@gmail.com>
2020-10-27 00:41:06 -0400

    Login most of the way working

--- src/components/login/LoginPage.vue
+++ src/components/login/LoginPage.vue
@@ -1,10 +1,11 @@
 <template>
   <div id='login-page'>
-    Login<br>
+    <h1>Login</h1>
     <input id='emailInput' placeholder='E-Mail' type='text' /><br>
-    <div id='email-errors'>{{ emailErrors }}</div>
+    <div id='email-errors' class='error'>{{ emailErrors }}</div>
     <input @focus='passFocus' id='passInput' placeholder='Password' type='text' /><br>
-    <div id='password-errors'>{{ passwordErrors }}</div>
+    <div id='password-errors' class='error'>{{ passwordErrors }}</div>
+    <div id='other-errors' class='error'>{{ otherErrors }}</div>
     <button @click='login' id='loginButton'>Login</button>
   </div>
 </template>
@@ -17,21 +18,32 @@ export default {
         return{
             emailErrors: "",
             passwordErrors: "",
+            otherErrors: ""
         }
     },
     methods: {
         passFocus: () =>{
             document.getElementById("passInput").type = 'password'
         },
-        login: async ()=>{
+        async login(){
             var email = document.getElementById('emailInput').value
             var pass = document.getElementById('passInput').value
-            processLogin(email, pass)
+            var errors = await processLogin(email, pass)
+            if (errors !== null){
+                this.emailErrors = errors[0]
+                this.passwordErrors = errors[1]
+                this.otherErrors = errors[2]
+            }
         }
     }
 }
 </script>
 
 <style>
-
+#login-page{
+    text-align: center;
+}
+.error{
+    color: red;
+}
 </style>
--- src/components/login/processLogin.js
+++ src/components/login/processLogin.js
@@ -1,15 +1,35 @@
-export default (email, password)=>{
+export default async (email, password)=>{
+    // Create regex patterns for email and password
     var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     var passwordRegex = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/
-
+    // Check and see if the email and password match the regex patterns
     var emailResult = emailRegex.test(email)
     var passResult = passwordRegex.test(password)
 
     var errors = ["", "", ""]
-    if (!emailResult){
-        errors[0] = "Email is not valid"
+    if(!emailResult || !passResult){
+        if (!emailResult){
+            errors[0] = "Email is not valid"
+        }
+        if (!passResult){
+            errors[1] = "Password is not valid"
+        }
+        return errors
+    }else{
+        // If all values look valid make a call to the API
+        const resp = await fetch('http://localhost:5000/login',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email:email,
+                pass:password
+            })
+        })
+        const respJSON = await resp.json()
+        console.log(respJSON)
+        return null
     }
-    if (!passResult){
-        errors[1] = "Password is not valid"
-    }
+    
 }
